f68de5fa332a83be27c0a46c001ac918fab2282a
elytrafae <48065803+elytrafae@users.noreply.github.com>
2024-07-20 10:40:17 +0300

    Frenzy!!!

---
+++ Systems/WhipFrenzy/WhipFrenzyGlobalItem.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FaeReforges.Systems.WhipFrenzy {
+    internal class WhipFrenzyGlobalItem : GlobalItem {
+
+        public override bool? UseItem(Item item, Player player) {
+            if (player.altFunctionUse == 2) {
+                player.WhipFrenzyPlayer().ActivateFrenzy();
+                return false;
+            }
+
+            /*
+            int projectile = Projectile.NewProjectile(
+                            player.GetSource_ItemUse(item),
+                            player.Center,
+                            Vector2.Zero,
+                            ProjectileID.CoolWhip,
+                            (int)player.GetDamage(item.DamageType).ApplyTo(item.damage),
+                            player.GetKnockback(item.DamageType).ApplyTo(item.knockBack),
+                            player.whoAmI,
+                            ProjAIStyleID.Whip
+                            );*/
+
+            
+            // When a player uses a whip, use two other ones!
+            if (player.WhipFrenzyPlayer().IsFrenzyActive() && Main.myPlayer == player.whoAmI) {
+                int additionalWhipUsesLeft = 2;
+                int[] whipsUsedSoFar = new int[additionalWhipUsesLeft];
+                for (var i = 0; i <= 9; i++) { // Please find a better way to iterate through hotbar items
+                    Item hotbarItem = player.inventory[i];
+                    if (WhipFrenzyPlayer.IsEligibleWhip(hotbarItem) && !whipsUsedSoFar.Contains(hotbarItem.type) && hotbarItem.type != item.type) {
+                        Console.WriteLine("Additional Whip Found! " + hotbarItem.AffixName());
+                        whipsUsedSoFar[whipsUsedSoFar.Length - additionalWhipUsesLeft] = hotbarItem.type;
+                        additionalWhipUsesLeft--;
+                        Vector2 shootVel = Main.MouseWorld - player.Center;
+                        shootVel.Normalize();
+                        shootVel *= 0.5f;
+                        int projectile = Projectile.NewProjectile(
+                            player.GetSource_ItemUse(hotbarItem), 
+                            player.Center,
+                            shootVel,
+                            hotbarItem.shoot, 
+                            (int)player.GetDamage(hotbarItem.DamageType).ApplyTo(hotbarItem.damage),
+                            player.GetKnockback(hotbarItem.DamageType).ApplyTo(hotbarItem.knockBack),
+                            player.whoAmI,
+                            -100f
+                            );
+                        if (additionalWhipUsesLeft <= 0) {
+                            break;
+                        }
+                    }
+                }
+                
+            }
+            
+            return true;
+        }
+
+        // I don't like adding new keys, but I might have to add one ;_;
+        public override bool AltFunctionUse(Item item, Player player) {
+            return true;
+        }
+
+    }
+}
---
+++ Systems/WhipFrenzy/WhipFrenzyPlayer.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Chat;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace FaeReforges.Systems.WhipFrenzy {
+    public class WhipFrenzyPlayer : ModPlayer {
+
+        int frenzy = 0;
+        int maxFrenzy = 1000;
+        bool isFrenzyOn = false;
+
+        public bool IsFrenzyReady() {
+            return frenzy >= maxFrenzy && !isFrenzyOn;
+        }
+
+        public bool IsFrenzyActive() {
+            return isFrenzyOn;
+        }
+
+        public void ActivateFrenzy() { 
+            if (!this.IsFrenzyReady()) { return; }
+            frenzy = maxFrenzy;
+            isFrenzyOn = true;
+            // Add sound and visual effects
+        }
+
+        public static bool IsEligibleWhip(Item item) {
+            return item.shoot != ProjectileID.None && ProjectileID.Sets.IsAWhip[item.shoot];
+            return (item.DamageType == DamageClass.SummonMeleeSpeed || item.DamageType.GetEffectInheritance(DamageClass.SummonMeleeSpeed)) && item.shoot != ProjectileID.None;
+        }
+
+        public void StopFrenzy() {
+            isFrenzyOn = false;
+            frenzy = 0;
+            // Add sound and visual effects
+        }
+
+        public override bool CanUseItem(Item item) {
+            if (IsFrenzyActive()) {
+                return IsEligibleWhip(item);
+            }
+            return true;
+        }
+
+
+        public override void PostUpdateEquips() {
+            if (IsFrenzyActive()) {
+                Player.GetAttackSpeed(DamageClass.SummonMeleeSpeed) += 0.2f;
+            }
+        }
+
+        public override void PostUpdate() {
+            if (IsFrenzyActive()) {
+                frenzy -= 5;
+                if (frenzy <= 0) {
+                    StopFrenzy();
+                }
+            } else {
+                // Passive Whip Frenzy Gain for DEBUG!
+                frenzy += 2;
+
+            }
+            ChatHelper.BroadcastChatMessage(NetworkText.FromLiteral("Frenzy: " + frenzy + "/" + maxFrenzy), Color.Azure);
+        }
+
+    }
+
+    public static class PlayerExtension { 
+    
+        public static WhipFrenzyPlayer WhipFrenzyPlayer(this Player player) { return player.GetModPlayer<WhipFrenzyPlayer>(); }
+
+    }
+}
