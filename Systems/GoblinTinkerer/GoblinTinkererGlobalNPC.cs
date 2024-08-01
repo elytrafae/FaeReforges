77a29072a4f6640fe4e3f0de670802e89cd754ec
elytrafae <48065803+elytrafae@users.noreply.github.com>
2024-08-01 21:53:28 +0300

    Hammer idea

---
+++ Systems/GoblinTinkerer/GoblinTinkererGlobalNPC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FaeReforges.Systems.GoblinTinkerer {
+    public class GoblinTinkererGlobalNPC : GlobalNPC {
+
+        public override bool AppliesToEntity(NPC entity, bool lateInstantiation) {
+            return entity.type == NPCID.GoblinTinkerer;
+        }
+
+         
+
+    }
+}
