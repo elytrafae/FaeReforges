95f1575a9112419bb2b9ce95fe4a4bfdc593ce73
elytrafae <48065803+elytrafae@users.noreply.github.com>
2025-04-11 19:41:41 +0300

    Spectre Hammer added

---
+++ Content/Items/TinkererHammers/Tier3/SpectreTinkererHammer.cs
@@ -0,0 +1,56 @@
+﻿using FaeLibrary.API.ItemConditions;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using FaeReforges.Content.Projectiles;
+using Microsoft.Xna.Framework;
+
+namespace FaeReforges.Content.Items.TinkererHammers.Tier3 {
+    public class SpectreTinkererHammer : SimpleTinkererHammerItem {
+        public override int Rarity => ItemRarityID.Lime;
+        public override int Value => Terraria.Item.buyPrice(gold: 2, silver: 30);
+        public override int HammerTier => 3;
+        public override ItemCondition ReforgeableCondition => ItemCondition.IsWeapon;
+
+        public override void HammerOnWeaponDealDamageNpc(int item, Player attacker, NPC victim, NPC.HitInfo hitInfo, int damageDone) {
+            // Enemy is dead!
+            if (victim.life <= 0) {
+                SpawnProjectile(victim.Center, item, attacker);
+            }
+        }
+
+        public override void HammerOnWeaponDealDamagePvp(int item, Player attacker, Player victim, Player.HurtInfo hurtInfo) {
+            // Enemy is dead!
+            if (victim.statLife <= 0) {
+                SpawnProjectile(victim.Center, item, attacker);
+            }
+        }
+
+        private void SpawnProjectile(Vector2 position, int itemID, Player attacker) {
+            if (attacker.whoAmI != Main.myPlayer) {
+                return;
+            }
+            int projType = ModContent.ProjectileType<FriendlyDungeonSpirit>();
+            if (attacker.ownedProjectileCounts[projType] >= 3) {
+                return;
+            }
+            int baseDamage = 50;
+            float baseKB = 2.5f;
+            DamageClass damageType = ContentSamples.ItemsByType[itemID].DamageType;
+            Projectile proj = Projectile.NewProjectileDirect(attacker.GetSource_FromThis(), position, Vector2.Zero, projType, (int)attacker.GetDamage(damageType).ApplyTo(baseDamage), attacker.GetKnockback(damageType).ApplyTo(baseKB), attacker.whoAmI);
+            if (proj != null && proj.active) {
+                proj.originalDamage = baseDamage;
+                proj.DamageType = damageType;
+            }
+        }
+
+        public override void AddRecipes() {
+            CreateRecipe()
+                .AddIngredient(ItemID.SpectreBar, 15)
+                .AddIngredient(ItemID.RichMahogany, 15)
+                .AddIngredient(ItemID.Rope, 5)
+                .AddTile<Content.Tiles.TinkererAnvil>()
+                .Register();
+        }
+    }
+}
---
+++ Content/Projectiles/FriendlyDungeonSpirit.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FaeReforges.Content.Projectiles {
+    public class FriendlyDungeonSpirit : ModProjectile {
+
+        public override void SetStaticDefaults() {
+            Main.projFrames[Type] = 3;
+            ProjectileID.Sets.CultistIsResistantTo[Type] = true;
+            Main.projPet[Type] = true;
+            ProjectileID.Sets.MinionTargettingFeature[Projectile.type] = true;
+        }
+
+        public override void SetDefaults() {
+            Projectile.penetrate = 3;
+            Projectile.width = 30;
+            Projectile.height = 30;
+            Projectile.frame = 0;
+            Projectile.frameCounter = 0;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.minion = true;
+            Projectile.minionSlots = 0;
+            Projectile.timeLeft = 10 * 60;
+            Projectile.tileCollide = false;
+            Projectile.ArmorPenetration = 10;
+            Projectile.OriginalArmorPenetration = 10;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 20;
+        }
+
+        public override void AI() {
+            if (!Projectile.TryGetOwner(out Player owner)) {
+                return;
+            }
+
+            SearchForTargets(owner, out bool foundTarget, out float distanceFromTarget, out Vector2 targetCenter);
+            Movement(foundTarget, distanceFromTarget, targetCenter);
+            Visuals();
+        }
+
+        // Here you can decide if your minion breaks things like grass or pots
+        public override bool? CanCutTiles() {
+            return false;
+        }
+
+        // This is mandatory if your minion deals contact damage (further related stuff in AI() in the Movement region)
+        public override bool MinionContactDamage() {
+            return true;
+        }
+
+        // Code taken from ExampleMod
+        private void SearchForTargets(Player owner, out bool foundTarget, out float distanceFromTarget, out Vector2 targetCenter) {
+            // Starting search distance
+            distanceFromTarget = 700f;
+            targetCenter = Projectile.position;
+            foundTarget = false;
+
+            // This code is required if your minion weapon has the targeting feature
+            if (owner.HasMinionAttackTargetNPC) {
+                NPC npc = Main.npc[owner.MinionAttackTargetNPC];
+                float between = Vector2.Distance(npc.Center, Projectile.Center);
+
+                // Reasonable distance away so it doesn't target across multiple screens
+                if (between < 2000f) {
+                    distanceFromTarget = between;
+                    targetCenter = npc.Center;
+                    foundTarget = true;
+                }
+            }
+
+            if (!foundTarget) {
+                // This code is required either way, used for finding a target
+                foreach (var npc in Main.ActiveNPCs) {
+                    if (npc.CanBeChasedBy()) {
+                        float between = Vector2.Distance(npc.Center, Projectile.Center);
+                        bool closest = Vector2.Distance(Projectile.Center, targetCenter) > between;
+                        bool inRange = between < distanceFromTarget;
+                        bool lineOfSight = Collision.CanHitLine(Projectile.position, Projectile.width, Projectile.height, npc.position, npc.width, npc.height);
+                        // Additional check for this specific minion behavior, otherwise it will stop attacking once it dashed through an enemy while flying though tiles afterwards
+                        // The number depends on various parameters seen in the movement code below. Test different ones out until it works alright
+                        bool closeThroughWall = between < 100f;
+
+                        if (((closest && inRange) || !foundTarget) && (lineOfSight || closeThroughWall)) {
+                            distanceFromTarget = between;
+                            targetCenter = npc.Center;
+                            foundTarget = true;
+                        }
+                    }
+                }
+            }
+
+            // friendly needs to be set to true so the minion can deal contact damage
+            // friendly needs to be set to false so it doesn't damage things like target dummies while idling
+            // Both things depend on if it has a target or not, so it's just one assignment here
+            // You don't need this assignment if your minion is shooting things instead of dealing contact damage
+            Projectile.friendly = foundTarget;
+        }
+
+
+        // Code taken from ExampleMod, with the small change that the Dungeon Spirit will slow down and "freeze" if there is no target, similar to the Snowflake minion
+        private void Movement(bool foundTarget, float distanceFromTarget, Vector2 targetCenter) {
+            // Default movement parameters (here for attacking)
+            float speed = 8f;
+            float inertia = 20f;
+
+            if (foundTarget) {
+                // Minion has a target: attack (here, fly towards the enemy)
+                if (distanceFromTarget > 40f) {
+                    // The immediate range around the target (so it doesn't latch onto it when close)
+                    Vector2 direction = targetCenter - Projectile.Center;
+                    direction.Normalize();
+                    direction *= speed;
+
+                    Projectile.velocity = (Projectile.velocity * (inertia - 1) + direction) / inertia;
+                }
+            } else {
+                Projectile.velocity /= 1.4f;
+            }
+        }
+
+        private void Visuals() {
+            // Animate sprite
+            if (++Projectile.frameCounter >= 5) {
+                Projectile.frameCounter = 0;
+                Projectile.frame = ++Projectile.frame % Main.projFrames[Projectile.type];
+            }
+
+            Projectile.rotation = Projectile.velocity.ToRotation();
+
+            // Some visuals here
+            Lighting.AddLight(Projectile.Center, Color.Green.ToVector3() * 0.78f);
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
+            OnHitEffects();
+        }
+
+        public override void OnHitPlayer(Player target, Player.HurtInfo info) {
+            OnHitEffects();
+        }
+
+        private void OnHitEffects() {
+            for (int i = 0; i < 5; i++) {
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GreenFairy);
+            }
+            SoundEngine.PlaySound(SoundID.NPCHit36.WithVolumeScale(0.6f), Projectile.Center);
+        }
+
+        public override void OnKill(int timeLeft) {
+            if (timeLeft <= 0) {
+                SoundEngine.PlaySound(SoundID.NPCDeath39.WithVolumeScale(0.8f), Projectile.Center);
+            }
+            
+            for (int i = 0; i < 15; i++) {
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GreenFairy);
+            }
+        }
+
+    }
+}
