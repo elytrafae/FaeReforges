d794c24df7f2b7f09176732c2545729cdc956bbe
elytrafae <48065803+elytrafae@users.noreply.github.com>
2025-05-07 22:54:52 +0300

    Reworked yesterday's code to be much better

--- Systems/ShopsAndLoot/BossLootSystem.cs
+++ Systems/ShopsAndLoot/BossLootSystem.cs
@@ -1,25 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using FaeReforges.Content.Items.TinkererHammers.Tier4;
 using Terraria;
 using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace FaeReforges.Systems.ShopsAndLoot {
     internal class BossLootSystem : ModSystem {
 
+        private static Dictionary<int, List<IItemDropRule>> NPCLootList = new();
+        private static Dictionary<int, List<IItemDropRule>> ItemLootList = new();
+        private static bool AreListsInitialized = false;
 
-        // TODO: Try to make this much more performant
-        // Current idea: Make these lists sorted by ID, binary search the ID, and then sequentially go forward and backward
-        // until you bump into the start/end of the list or a different ID
-        public static List<Tuple<int, IItemDropRule>> NPCLootList = new();
-        public static List<Tuple<int, IItemDropRule>> ItemLootList = new();
+        public override void Unload() {
+            NPCLootList.Clear();
+            ItemLootList.Clear();
+            AreListsInitialized = false;
+        }
 
-        public void AddLootToBossAndBag(int npcID, int itemID, IItemDropRule rule) {
+        public static void AddLoot() {
+            AddLootToBossAndBag(NPCID.DD2Betsy, ItemID.BossBagBetsy, ItemDropRule.Common(ModContent.ItemType<BetsyTinkererHammer>()));
+        }
+
+        public static void AddLootToItem(int itemID, IItemDropRule rule) {
+            if (!ItemLootList.TryGetValue(itemID, out List<IItemDropRule> ruleList)) { 
+                ruleList = [];
+            }
+            ruleList.Add(rule);
+        }
+
+        public static void AddLootToNPC(int npcID, IItemDropRule rule) {
+            if (!NPCLootList.TryGetValue(npcID, out List<IItemDropRule> ruleList)) {
+                ruleList = [];
+            }
+            ruleList.Add(rule);
+        }
+
+        public static void AddLootToBossAndBag(int npcID, int itemID, IItemDropRule rule) {
             LeadingConditionRule notExpertRule = new LeadingConditionRule(new Conditions.NotExpert());
             notExpertRule.OnSuccess(rule);
 
-            NPCLootList.Add(new(npcID, notExpertRule));
-            ItemLootList.Add(new(itemID, rule));
+            AddLootToNPC(npcID, notExpertRule);
+            AddLootToItem(itemID, rule);
+        }
+
+        public static void InitializeLists() {
+            if (AreListsInitialized) {
+                return;
+            }
+            AreListsInitialized = true;
+            AddLoot();
+
+        }
+
+        // This code gives me school dread... And I wrote it!
+        private static void IterateID(int id, ILoot loot, Dictionary<int, List<IItemDropRule>> dict) {
+            if (dict.TryGetValue(id, out List<IItemDropRule> list)) {
+                foreach (IItemDropRule rule in list) { 
+                    loot.Add(rule);
+                }
+            }
+        }
+
+        public static void IterateNPCID(int id, NPCLoot loot) {
+            IterateID(id, loot, NPCLootList);
+        }
+
+        public static void IterateItemID(int id, ItemLoot loot) {
+            IterateID(id, loot, ItemLootList);
         }
 
     }
@@ -27,11 +76,8 @@ namespace FaeReforges.Systems.ShopsAndLoot {
     internal class BossLootNPC : GlobalNPC {
 
         public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot) {
-            foreach (Tuple<int, IItemDropRule> tuple in BossLootSystem.NPCLootList) {
-                if (tuple.Item1 == npc.type) {
-                    npcLoot.Add(tuple.Item2);
-                }
-            }
+            BossLootSystem.InitializeLists();
+            BossLootSystem.IterateNPCID(npc.type, npcLoot);
         }
 
     }
@@ -39,11 +85,8 @@ namespace FaeReforges.Systems.ShopsAndLoot {
     internal class BossLootItem : GlobalItem {
 
         public override void ModifyItemLoot(Item item, ItemLoot itemLoot) {
-            foreach (Tuple<int, IItemDropRule> tuple in BossLootSystem.ItemLootList) {
-                if (tuple.Item1 == item.type) {
-                    itemLoot.Add(tuple.Item2);
-                }
-            }
+            BossLootSystem.InitializeLists();
+            BossLootSystem.IterateItemID(item.type, itemLoot);
         }
 
     }
